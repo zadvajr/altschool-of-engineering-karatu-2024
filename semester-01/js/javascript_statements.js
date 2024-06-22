e20d3b4077b43435eeb071bb306646294be85cfa
zadvajr <danielzadva@gmail.com>
2024-06-23 00:25:27 +0100

    js statements

---
+++ semester-01/js/javascript_statements.js
@@ -0,0 +1,12 @@
+//this is a comment
+/* this is also a comment */
+// this section is about javascript statements
+let x, y, z; //declare multiple variables on a single line
+
+//initializes variables
+x = 5;
+y = 6;
+
+z = x + y; // sums up the values store in x and y and assigns it to z
+
+console.log(z); //ptints 11
---
+++ semester-01/js/js_variables.js
@@ -0,0 +1,34 @@
+//javascript variables
+//there are for ways to declare variables in js
+
+//automatically
+x = 5;
+y = 6;
+z = x + y;
+console.log(z); //prints 11
+
+//end
+
+//using var keyword
+var x = 5;
+var y = 6;
+var z = x + y;
+console.log(z); //prints 11
+
+//end
+
+//usint let keyword
+let a = 5;
+let b = 6;
+let c = a + b;
+console.log(c); //prints 11
+
+//end
+
+//using const keyword
+const i = 10;
+const j = 12;
+const k = i + j;
+console.log(k); //prints 22
+
+//end
