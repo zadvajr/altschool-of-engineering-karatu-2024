0de6afe6d8d63dc60c4582d29e46a91ed5322103
zadvajr <danielzadva@gmail.com>
2024-06-23 12:46:08 +0100

    javascript strings

---
+++ semester-01/js/js_strings.js
@@ -0,0 +1,8 @@
+//javascript strings
+//strings are zero or more characters inside single or double quotes
+
+//creating string
+let name = "John Doe";
+
+//string length: finding the length of a string
+console.log("there are " + name.length + " characters in " + name);
